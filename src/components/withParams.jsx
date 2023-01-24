f3014888d9a75e90256f32c9cba96bd2403f3c1e
임현정 | Alex Im <84281505+h-alex2@users.noreply.github.com>
2023-01-25 00:00:07 +0900

    Feat withParams hoc 컴포넌트 추가 react router useParams 쓰기 위해 추가

---
+++ src/components/withParams.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
+const withParams = Child => {
+  return function (props) {
+    const params = useParams();
+    return <Child {...props} params={params} />;
+  };
+};
+
+export default withParams;
