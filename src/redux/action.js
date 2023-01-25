af5ea8c8984872a911f16e2ca84d4f69dcfcb877
임현정 | Alex Im <84281505+h-alex2@users.noreply.github.com>
2023-01-25 23:01:24 +0900

    Feat action 추가

--- src/redux/action.js
+++ src/redux/action.js
@@ -1,8 +1,31 @@
-import { SELECT_MODE, CLICK_CELL } from "./actionTypes";
+import {
+  START_GAME,
+  CLICK_CELL,
+  STICK_MINE_FLAG,
+  STICK_QUESTION_MARK,
+  RESET_CELL,
+} from "./actionTypes";
 
-export const selectMode = mode => ({ type: SELECT_MODE, payload: { mode } });
+export const startGame = mode => ({ type: START_GAME, payload: { mode } });
 
 export const clickCell = clickedCellIndex => ({
   type: CLICK_CELL,
+  payload: { clickedCellIndex },
+});
+
+export const stickMineFlag = clickedCellIndex => {
+  return {
+    type: STICK_MINE_FLAG,
+    payload: { clickedCellIndex },
+  };
+};
+
+export const stickQuestionMark = clickedCellIndex => ({
+  type: STICK_QUESTION_MARK,
+  payload: { clickedCellIndex },
+});
+
+export const resetCell = clickedCellIndex => ({
+  type: RESET_CELL,
   payload: { clickedCellIndex },
 });
--- src/redux/actionTypes.js
+++ src/redux/actionTypes.js
@@ -1,2 +1,5 @@
-export const SELECT_MODE = "SELECT_MODE";
+export const START_GAME = "START_GAME";
 export const CLICK_CELL = "CLICK_CELL";
+export const STICK_MINE_FLAG = "STICK_MINE_FLAG";
+export const STICK_QUESTION_MARK = "STICK_QUESTION_MARK";
+export const RESET_CELL = "RESET_CELL";
