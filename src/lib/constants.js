47b97c85a06cda33e11a43641b56179eaafb67f7
임현정 | Alex Im <84281505+h-alex2@users.noreply.github.com>
2023-01-25 23:02:17 +0900

    Feat 상수 추가, 네이밍 변경

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,34 +1,35 @@
 export const MODES = {
-  EASY: {
-    X: 5,
-    Y: 6,
-    MINES: 10,
+  easy: {
+    ROW: 5,
+    COL: 6,
+    MINES_COUNT: 3,
   },
-  MEDIUM: {
-    X: 16,
-    Y: 16,
-    MINES: 40,
+  medium: {
+    ROW: 16,
+    COL: 16,
+    MINES_COUNT: 40,
   },
-  HARD: {
-    X: 30,
-    Y: 16,
-    MINES: 99,
+  hard: {
+    ROW: 30,
+    COL: 16,
+    MINES_COUNT: 99,
   },
-  EXTREAM: {
-    X: 30,
-    Y: 24,
-    MINES: 180,
+  extream: {
+    ROW: 30,
+    COL: 24,
+    MINES_COUNT: 180,
   },
-  CUSTOM: {
-    X: 18,
-    Y: 9,
-    MINES: 24,
+  custom: {
+    ROW: 18,
+    COL: 9,
+    MINES_COUNT: 24,
   },
 };
 
 export const FLAG = {
   MINE: -1,
-  MINE_FLAG: 11,
+  MINE_FLAG: 13,
+  QUESTION_MARK: 12,
   EMPTY: 10,
   OPENED: 9,
   AROUND_ALL_EMPTY: 8,
@@ -37,4 +38,10 @@ export const FLAG = {
 export const GAME_STATUS = {
   PROCEEDING: "Proceeding",
   FAIL: "Fail",
+  SUCCESS: "success",
+};
+
+export const MOUSE = {
+  RIGHT_MOUSE: 2,
+  LEFT_MOUSE: 0,
 };
