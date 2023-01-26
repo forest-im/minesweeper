db5526bb7958b697af7c92865eaa3930948fd3c7
임현정 | Alex Im <84281505+h-alex2@users.noreply.github.com>
2023-01-27 06:38:52 +0900

    Feat timer 초기화 기능 추가

--- src/container/Timer.jsx
+++ src/container/Timer.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import PropTypes from "prop-types";
+import styleConfig from "../styleConfig";
 
 class Timer extends React.Component {
   timerId;
@@ -16,14 +18,47 @@ class Timer extends React.Component {
     }, 1000);
   }
 
+  componentDidUpdate() {
+    const { onTimeOut, gameStatus } = this.props;
+    const { timer } = this.state;
+
+    if (timer === 999 || gameStatus === "Fail") {
+      onTimeOut();
+      clearInterval(this.timerId);
+      this.timerId = 0;
+    }
+
+    if (!this.timerId && gameStatus === "Proceeding") {
+      this.setState({ timer: 0 });
+
+      this.timerId = setInterval(() => {
+        this.setState(prev => {
+          return { timer: prev.timer + 1 };
+        });
+      }, 1000);
+    }
+  }
+
   componentWillUnmount() {
     clearInterval(this.timerId);
   }
 
   render() {
     const { timer } = this.state;
-    return <div>{timer}</div>;
+    return (
+      <div>
+        <div className={`${styleConfig.header.base} grid-cols-2`}>
+          <span>🕰</span>
+          <span>{timer}</span>
+        </div>
+      </div>
+    );
   }
 }
+
+Timer.propTypes = {
+  onTimeOut: PropTypes.func.isRequired,
+  gameStatus: PropTypes.string.isRequired,
+};
 
 export default Timer;
