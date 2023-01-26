c848fd688c84e9d007c136ba4aec5033b9d29f0c
임현정 | Alex Im <84281505+h-alex2@users.noreply.github.com>
2023-01-27 06:40:27 +0900

    Chore 상수 추가, 수정

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -2,7 +2,7 @@ export const MODES = {
   easy: {
     ROW: 5,
     COL: 6,
-    MINES_COUNT: 3,
+    MINES_COUNT: 10,
   },
   medium: {
     ROW: 16,
@@ -12,33 +12,46 @@ export const MODES = {
   hard: {
     ROW: 30,
     COL: 16,
-    MINES_COUNT: 99,
+    MINES_COUNT: 50,
   },
   extream: {
     ROW: 30,
     COL: 24,
     MINES_COUNT: 180,
   },
-  custom: {
-    ROW: 18,
-    COL: 9,
-    MINES_COUNT: 24,
-  },
 };
 
 export const FLAG = {
   MINE: -1,
-  MINE_FLAG: 13,
-  QUESTION_MARK: 12,
+  AROUND_ALL_EMPTY: 9,
   EMPTY: 10,
-  OPENED: 9,
-  AROUND_ALL_EMPTY: 8,
+  OPENED: 11,
+  QUESTION_MARK: 12,
+  MINE_FLAG: 13,
+};
+
+export const FLAG_CONTENT = {
+  1: 1,
+  2: 2,
+  3: 3,
+  4: 4,
+  5: 5,
+  6: 6,
+  7: 7,
+  8: 8,
+  9: "",
+  10: "",
+  11: "",
+  12: "❔",
+  13: "🏳️‍🌈",
+  [-1]: "",
+  mine: "💣",
 };
 
 export const GAME_STATUS = {
   PROCEEDING: "Proceeding",
   FAIL: "Fail",
-  SUCCESS: "success",
+  SUCCESS: "Success",
 };
 
 export const MOUSE = {
