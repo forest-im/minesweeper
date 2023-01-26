24e920030de4b30053baa1d0a69d0655b3e9d608
임현정 | Alex Im <84281505+h-alex2@users.noreply.github.com>
2023-01-27 06:41:55 +0900

    Chore css 업데이트, state 추가

--- src/container/Header.jsx
+++ src/container/Header.jsx
@@ -1,30 +1,118 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 import Timer from "./Timer";
+import { startGame, timeOut, toggleModal } from "../redux/action";
+import styleConfig from "../styleConfig";
+import withParams from "../components/withParams";
 
 class Header extends React.PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      currentStatus: "😊",
+    };
+  }
+
+  componentDidUpdate() {
+    const { gameStatus } = this.props;
+
+    if (gameStatus === "Proceeding") return;
+
+    switch (gameStatus) {
+      case "Proceeding": {
+        return this.setState({ currentStatus: "😊" });
+      }
+      case "Fail": {
+        return this.setState({ currentStatus: "💀" });
+      }
+      case "Success": {
+        return this.setState({ currentStatus: "🤩" });
+      }
+      default: {
+        break;
+      }
+    }
+  }
+
   render() {
-    const { mode, minesCount } = this.props;
+    const {
+      params,
+      gameStatus,
+      minesCount,
+      isModalShowing,
+      dispatchTimeOut,
+      dispatchStartGame,
+      dispatchToggleModal,
+    } = this.props;
 
     return (
-      <div className="flex flex-row justify-between w-1/2 text-2xl border-solid border-2 border-sky-500 p-10">
-        <Timer />
-        <div>{mode}</div>
-        <div>😊</div>
-        <div>{minesCount}</div>
-      </div>
+      <>
+        <div className="grid grid-flow-col place-items-center gap-3 sm:gap-10">
+          <Link to="/">HOME</Link>
+          <button
+            className="cursor-pointer"
+            type="button"
+            onClick={dispatchToggleModal}
+          >
+            MENU
+          </button>
+          {gameStatus === "Fail" && (
+            <button
+              className="cursor-pointer"
+              onClick={() => dispatchStartGame(params.mode)}
+              type="button"
+            >
+              RETRY
+            </button>
+          )}
+        </div>
+        <div className="my-5 grid grid-cols-3 place-items-center gap-3 sm:gap-10">
+          <div className="grid place-items-center">
+            <Timer
+              isModalShowing={isModalShowing}
+              gameStatus={gameStatus}
+              onTimeOut={dispatchTimeOut}
+            />
+          </div>
+          <div>
+            <div className={styleConfig.header.base}>
+              {this.state.currentStatus}
+            </div>
+          </div>
+          <div className="grid place-items-center">
+            <div className={`${styleConfig.header.base} grid-cols-2`}>
+              <span>💣</span>
+              <span>{minesCount}</span>
+            </div>
+          </div>
+        </div>
+      </>
     );
   }
 }
 
 Header.propTypes = {
-  mode: PropTypes.string.isRequired,
+  params: PropTypes.object.isRequired,
+  gameStatus: PropTypes.string.isRequired,
   minesCount: PropTypes.number.isRequired,
+  isModalShowing: PropTypes.bool.isRequired,
+  dispatchTimeOut: PropTypes.func.isRequired,
+  dispatchStartGame: PropTypes.func.isRequired,
+  dispatchToggleModal: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = state => ({
   ...state.status,
+  ...state.base,
 });
 
-export default connect(mapStateToProps, null)(Header);
+const mapDispatchToProps = {
+  dispatchTimeOut: timeOut,
+  dispatchStartGame: startGame,
+  dispatchToggleModal: toggleModal,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(withParams(Header));
