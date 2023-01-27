861ebb741a327c88fd9a0599fb9efb9da57d9b3a
임현정 | Alex Im <84281505+h-alex2@users.noreply.github.com>
2023-01-27 11:21:50 +0900

    Chore 웰컴 모달 패딩 수정

--- src/components/WelcomeModal.jsx
+++ src/components/WelcomeModal.jsx
@@ -3,7 +3,7 @@ import React from "react";
 class WelcomeModal extends React.PureComponent {
   render() {
     return (
-      <div className="grid h-fit w-80 items-center rounded-3xl border-2 border-solid bg-white p-10  leading-10">
+      <div className="grid h-fit w-80 items-center rounded-3xl border-2 border-solid bg-white p-5  leading-10">
         <div className="mb-3 text-2xl font-bold">
           Welcome to Minesweeper game
         </div>
